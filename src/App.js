47be64a5ebae7a243f875609a9488a8e4f359be5
saihyny <satyasaikiranrocks@gmail.com>
2023-11-08 17:47:15 +0530

    commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,30 @@
-import logo from './logo.svg';
+
 import './App.css';
+import {BrowserRouter,Route,Routes,Link} from 'react-router-dom'
+import Home from "./pages/Home"
+import About from "./pages/About"
+import Vans from "./pages/Vans"
+import VanDetail from "./pages/VanDetail"
+
+import "./server"
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+    <header>
+      <Link className="site-logo" to="/">#VanLife</Link>
+      <nav>
+        <Link to="/about">About</Link>
+        <Link to="/vans">Vans</Link>
+      </nav>
+    </header>
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/about" element={<About />} />
+      <Route path="/vans" element={<Vans />} />
+      <Route path="/vans/:id" element={<VanDetail />} />
+    </Routes>
+  </BrowserRouter>
   );
 }
 
